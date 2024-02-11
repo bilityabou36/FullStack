b858a72d4c3e50a80abac18de588e5481b54dec8
abou bility <bilityabou36@gmail.com>
2024-02-11 15:18:13 -0600

    modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import Employee from './components/Employee'
-
+import { useState } from 'react';
+import Employee from './components/Employee';
+import './App.css';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
-  const[role, setRole] = useState('nurse')
+  const[role, setRole] = useState('subject')
   const isEmployee = true;
+  const [employees, setEmployees] = useState([{
+ name: "Abou",
+ role: "Mansa",
+ img: "https://cdn.pixabay.com/photo/2012/02/27/15/35/lion-17335_1280.jpg"
+  }, {
+    name: "Makessa",
+    role: "Queen",
+    img: "https://cdn.pixabay.com/photo/2019/12/10/13/31/woman-4685862_1280.jpg"
+  }, {
+    name: "Hassan",
+    role: "Prince",
+    img: "https://cdn.pixabay.com/photo/2019/04/03/13/45/prince-hal-4100357_1280.jpg"
+  }, {
+    name: "Abraham",
+    role: "Farma",
+    img: "https://cdn.pixabay.com/photo/2018/03/31/21/18/african-american-3279360_1280.jpg"
+  }])
 
   return (
-    <div>
+    <div className="app">
     {isEmployee ? (
-      <div>
+      <div >
          <input type="text"
     onChange={ (e)=> {
       console.log(e.target.value)
@@ -17,18 +35,16 @@ function App() {
     }}
     
     />
-    <Employee
-    name = "Abou"
-    role = "Mansa"
-    />
-    <Employee
-    name = "Makessa"
-    role = "Princess"/>
-    <Employee
-    name = "John"
-    role={role}
-    />
-   
+  <div className='flex flex-wrap'>
+    {employees.map((employee)=>{
+      console.log(employee)
+    return(
+    <Employee key={uuidv4()} name={employee.name} 
+    role={employee.role} img={employee.img}/>
+    )
+
+    })}
+  </div>
     
 
 </div>
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import EditEmployee from './EditEmployee'
 
 const Employee = (props) => {
   return (
     <>
-    <h3>Employee {props.name}</h3>
-    {props.role ? props.role : "no role"}
+  <div className=" m-2 py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img}  />
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        {props.name}
+      </p>
+      <p className="text-slate-500 font-medium">
+        {props.role}
+      </p>
+    </div>
+    <EditEmployee name={props.name}  role = {props.role}/>
+    
+  </div>
+</div>
 
     </>
   )
